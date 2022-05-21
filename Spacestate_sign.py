d3461357ce0fe0fd166c020c98a212afbc732abd
Kevin B <42434740+Tahtk3v@users.noreply.github.com>
2022-05-21 22:44:33 +0200

    Final code that is reactive on space state

---
+++ Spacestate_sign.py
@@ -0,0 +1,71 @@
+from ctypes import alignment
+import struct
+import socket
+import os
+from PIL import Image, ImageFont, ImageDraw
+import paho.mqtt.client 
+mqtt = paho.mqtt.client.Client()
+
+
+pixelBuffer = []
+rgbMode = struct.pack("<?", 0)
+versionBit = struct.pack("<B", 1)
+pixelvlut = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+def send_bufferfull(pixelBuffer):
+    buffer = versionBit + rgbMode
+    for buf in pixelBuffer:
+        buffer += buf
+    if len(buffer) >= 1400:
+        pixelvlut.sendto(buffer,  ("10.208.42.134", 5004))
+        return True
+    return False
+
+def show_image(image):
+    pixelBuffer = []
+    for x in range(image.width):
+        for y in range(image.height):
+            r, g, b = image.getpixel((x, y ))
+            pixelBuffer.append(bytes(struct.pack("<2H3B", x, y, r, g, b)))
+            if send_bufferfull(pixelBuffer):
+                    pixelBuffer = []
+
+def open():
+    image = Image.new("RGB", (64, 32))
+    font = ImageFont.truetype("Comfortaa-Regular.ttf", 12)
+    draw = ImageDraw.Draw(image)
+    draw.text((16,0), "We're\nOpen", font=font, alignment="center",fill=(0, 255, 0,)) 
+    show_image(image)
+
+def closed():
+    image = Image.new("RGB", (64, 32))
+    font = ImageFont.truetype("Comfortaa-Regular.ttf", 12)
+    draw = ImageDraw.Draw(image)
+    draw.text((13,0), "We're\nClosed", font=font, alignment="center",fill=(255, 0, 0,)) 
+    show_image(image)
+
+
+def setup_mqtt():
+    mqtt.on_connect = on_connect
+    mqtt.on_message = on_message
+    mqtt.on_disconnect = on_disconnect
+    mqtt.connect("jarvis.vm.nurd.space")
+
+def on_disconnect(client, userdata, rc):
+    setup_mqtt()
+
+def on_connect(client, userdata, flags, rc):
+    mqtt.subscribe("space/#")
+
+def on_message(client, userdata, msg):
+    
+    if msg.topic == "space/state":
+        spacestate = True if msg.payload.decode("utf-8") == "True" else False
+        if spacestate==True:
+            open()
+        else:
+            closed()
+        print(spacestate)
+
+setup_mqtt()
+mqtt.loop_forever()
